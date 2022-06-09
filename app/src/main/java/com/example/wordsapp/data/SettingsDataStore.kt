9b219d92f1f406c0faef5b26ba95767704672f2b
Xsufu <aluno.macarrao@gmail.com>
2022-06-09 13:11:15 +0700

    Добавлена обработка исключений

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -5,9 +5,12 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
@@ -24,6 +27,14 @@ class SettingsDataStore(context: Context) {
     }
 
     val preferenceFlow: Flow<Boolean> = context.dataStore.data
+        .catch {
+            if (it is IOException) {
+                it.printStackTrace()
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
         .map { preferences ->
             preferences[IS_LINEAR_LAYOUT_MANAGER] ?: true
         }
