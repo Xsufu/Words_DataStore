84620fcafeaa940d2f94016229078652ebcbac7f
Xsufu <aluno.macarrao@gmail.com>
2022-06-09 13:06:34 +0700

    Создана функция записи в хранилище данных

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -3,6 +3,8 @@ package com.example.wordsapp.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
@@ -13,4 +15,9 @@ private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
 class SettingsDataStore(context: Context) {
     private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
 
+    suspend fun saveLayoutToPreferencesStore(isLinearLayoutManager: Boolean, context: Context) {
+        context.dataStore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+        }
+    }
 }
